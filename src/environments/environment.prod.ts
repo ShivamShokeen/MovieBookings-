9a322b62bcf590cdd9bd89192f635fa533effe6c
ShivamShokeen <57745914+ShivamShokeen@users.noreply.github.com>
2020-10-17 12:10:30 +0530

    Minor changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCibFvqKuqG27psWVhYnYLTYgjxT-lG5WY",
+    authDomain: "moviebooking-35404.firebaseapp.com",
+    databaseURL: "https://moviebooking-35404.firebaseio.com",
+    projectId: "moviebooking-35404",
+    storageBucket: "moviebooking-35404.appspot.com",
+    messagingSenderId: "773056901404",
+    appId: "1:773056901404:web:0c7a659f660cd7d659cad9",
+    measurementId: "G-FL0VS5ZE4X"
+  }
 };
